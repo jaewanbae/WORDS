90758c60400557902e2976f119dde4b6b79a6c0f
code-by-mandy <mandycodes2@gmail.com>
2021-03-28 14:48:45 -0400

    if-else check that the word has at least one definition, get def and part of speech

--- scripts.js
+++ scripts.js
@@ -18,7 +18,8 @@ wordsApp.getWord = () => {
         .then((jsonResponse) => {
             //check word length is less than 8
             if (jsonResponse[0].length <= 8) {                        
-                // wordsApp.getDef(jsonResponse[0]);
+                console.log(jsonResponse[0]);                        
+                wordsApp.getDef(jsonResponse[0]);
                 wordsApp.getBoxes(jsonResponse[0].length);
             } else {
                 wordsApp.getWord();
@@ -32,23 +33,27 @@ wordsApp.getBoxes = (numOfBoxes) => {
 }
 
 //get random word definition
-// wordsApp.getDef = (randomWord) => {    
-//     merriamApiUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${randomWord}`;
-//     merriamApiKey = "10c2ccc6-00e7-4a3e-907d-d70bbfbacb4b";
-//     const url = new URL(merriamApiUrl);
+wordsApp.getDef = (randomWord) => {    
+    merriamApiUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${randomWord}`;
+    merriamApiKey = "10c2ccc6-00e7-4a3e-907d-d70bbfbacb4b";
+    const url = new URL(merriamApiUrl);
     
-//     url.search = new URLSearchParams({
-//         key: merriamApiKey
-//     })
+    url.search = new URLSearchParams({
+        key: merriamApiKey
+    })
 
-//     fetch(url)
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((jsonResponse) => {
-//             console.log(jsonResponse);
-//         })
-// }
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((jsonResponse) => {
+            //if no definition (undefined), get another word and its part of speech
+            if (jsonResponse[0].shortdef) {
+                console.log(jsonResponse[0].shortdef[0]);
+                console.log(jsonResponse[0].fl);
+            } else (wordsApp.getWord());
+        })
+}
 
 wordsApp.init = () => {
     // wordsApp.curtainRise();
