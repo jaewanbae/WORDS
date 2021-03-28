ff05a96a0e63c02193f838447ed2601269109f06
code-by-mandy <mandycodes2@gmail.com>
2021-03-28 13:42:19 -0400

    linked the two apis to get resulting merriam-webster array with definition of random word

--- scripts.js
+++ scripts.js
@@ -1,15 +1,47 @@
 wordsApp = {};
 
 // curtain JS
-wordsApp.curtainRise = () => {
-    const curtain = document.querySelector(`.curtain`)
-    curtain.addEventListener(`click`, () => {
-        curtain.classList.add(`topCurtainRise`)
+// wordsApp.curtainRise = () => {
+//     const curtain = document.querySelector(`.curtain`)
+//     curtain.addEventListener(`click`, () => {
+//         curtain.classList.add(`topCurtainRise`)
+//     })
+// }
+
+//get random word
+wordsApp.getWord = () => {
+    wordsApp.randomWordApiUrl = "https://random-word-api.herokuapp.com/word?number=1";
+    fetch(wordsApp.randomWordApiUrl)
+        .then((response) => {
+            return response.json();
+        })
+        .then((jsonResponse) => {
+            wordsApp.getDef(jsonResponse[0]);
+        })
+}
+
+//get random word definition
+wordsApp.getDef = (randomWord) => {    
+    merriamApiUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${randomWord}`;
+    merriamApiKey = "10c2ccc6-00e7-4a3e-907d-d70bbfbacb4b";
+    const url = new URL(merriamApiUrl);
+    
+    url.search = new URLSearchParams({
+        key: merriamApiKey
     })
+
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((jsonResponse) => {
+            console.log(jsonResponse);
+        })
 }
 
 wordsApp.init = () => {
-    wordsApp.curtainRise();
+    // wordsApp.curtainRise();
+    wordsApp.getWord();
 }
 
 wordsApp.init();
