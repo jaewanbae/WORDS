620c8beb91e2577f67c2902a30faead159bfc4bd
code-by-mandy <mandycodes2@gmail.com>
2021-03-28 23:29:52 -0400

    show boxes per word length

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ wordsApp.getWord = () => {
         })
 }
 
-//get random word definition
+//get random word with definition
 wordsApp.getDef = (randomWord) => {    
     merriamApiUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${randomWord}`;
     merriamApiKey = "10c2ccc6-00e7-4a3e-907d-d70bbfbacb4b";
@@ -40,21 +40,34 @@ wordsApp.getDef = (randomWord) => {
             return response.json();
         })
         .then((jsonResponse) => {
-            console.log(randomWord);
-            //if word is in dictionary and has short definition, console.log
-            console.log(jsonResponse[0].shortdef[0]);
+            // //console.log word given by first api
+            // console.log(randomWord);
+            //call hintOne() assuming word is in dictionary and has short definition 
+            wordsApp.giveHintOne(jsonResponse[0].shortdef[0]);
             //then get word length
-            wordsApp.getBoxes(randomWord.length);
-            // console.log(jsonResponse[0].fl);
+            wordsApp.getLetterBoxes(randomWord.length);
+            console.log(randomWord);
+            // console.log(jsonResponse[0].fl); -- hint for part of speech
             // if cors array (word isn't in dictionary) or word has no short definition, get another word 
             }).catch ((error) => {
                 wordsApp.getWord();
             })
 }
 
+//give hint one
+wordsApp.giveHintOne = (hintOne) => {
+    const definition = document.querySelector('.definition');
+    definition.innerText = `${hintOne}`;
+}
+
 //get number of boxes per word length
-wordsApp.getBoxes = (numOfBoxes) => {
-    console.log(numOfBoxes);
+wordsApp.getLetterBoxes = (numOfBoxes) => {
+    const letterBoxes = document.querySelectorAll('.letter');
+    const letterBoxesArray = Array.from(letterBoxes);
+    const letterBoxesInPlay = letterBoxesArray.slice(0, numOfBoxes);
+    for (let letterBox of letterBoxesInPlay) {
+        letterBox.classList.add("inPlay");
+      }      
 }
 
 wordsApp.init = () => {
